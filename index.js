b81f1ee118094f40d2d6d328216c5f480a1a9bfd
khan <khanmohammad2310@gmail.com>
2019-03-04 18:11:45 +0600

    fixed user's wrong input warning message

--- index.js
+++ index.js
@@ -6,15 +6,14 @@ const BASEURL = "https://api.apixu.com/v1";
 const KEY = "0059b84ab2ae4899afa52938190303";
 
 getWeatherButton.addEventListener("click", func => {
-  if(inputLocationText.value != ""){
+  if (inputLocationText.value != "") {
     requestWeatherInfo();
   }
-
 });
 
 inputLocationText.addEventListener("keyup", func => {
   if (func.keyCode === 13) {
-    if(inputLocationText.value != ""){
+    if (inputLocationText.value != "") {
       requestWeatherInfo();
     }
   }
@@ -22,31 +21,45 @@ inputLocationText.addEventListener("keyup", func => {
 
 function requestWeatherInfo() {
   const LOCATION = inputLocationText.value;
-  console.log(LOCATION, "location value");
   fetch(`${BASEURL}/forecast.json?key=${KEY}&q=${LOCATION}&days=${5}`)
-    .then(response => response.json())
+    .then(response => {
+      if (!response.ok) {
+        errorMsg("Please enter correct location");
+      }
+      return response.json();
+    })
     .then(forcast => showWeatherForcast(forcast))
     .catch(err => {
-      console.log(err);
+      console.log(err,"catch error");
     });
 }
 
+function errorMsg(err) {
+  let tagP = document.createElement("P");
+  tagP.className = "error";
+  let textNode = document.createTextNode(err);
+  showWeatherResult.appendChild(tagP);
+  tagP.appendChild(textNode);
+}
+
 function showWeatherForcast(forcast) {
-  let location = `Location :  ${forcast.location.name}, ${forcast.location.country}`;
-  let locationTime= `Time :  ${forcast.location.localtime}`;
+  let location = `Location :  ${forcast.location.name}, ${
+    forcast.location.country
+  }`;
+  let locationTime = `Time :  ${forcast.location.localtime}`;
   let condition = `Condition :  ${forcast.current.condition.text}`;
   let temperature = `Temperature :  ${forcast.current.temp_c} \u00B0C`;
   let humidity = `Humidity :  ${forcast.current.humidity}%`;
   let windkph = `Wind :  ${forcast.current.wind_kph} km/h`;
 
-  setWeatherInfo(location,"location");
-  setWeatherInfo(locationTime,"dada");
-  setWeatherInfo(condition,"dada");
-  setWeatherInfo(temperature,"dada");
-  setWeatherInfo(humidity,"dada");
-  setWeatherInfo(windkph,"dada");
+  setWeatherInfo(location, "location");
+  setWeatherInfo(locationTime, "dada");
+  setWeatherInfo(condition, "dada");
+  setWeatherInfo(temperature, "dada");
+  setWeatherInfo(humidity, "dada");
+  setWeatherInfo(windkph, "dada");
 
-  function setWeatherInfo(weatherValue, className){
+  function setWeatherInfo(weatherValue, className) {
     let tagP = document.createElement("P");
     tagP.className = className;
     let textNode = document.createTextNode(weatherValue);
